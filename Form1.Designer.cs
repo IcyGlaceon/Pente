f0b46db0d9c54f03b674988fe5329df05ff016e7
Carter Small <melaniesmall461@gmail.com>
2024-02-01 10:42:57 -0700

    Basics

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            PenteTB = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // PenteTB
+            // 
+            PenteTB.ColumnCount = 19;
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.26315641F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5.263157F));
+            PenteTB.Location = new Point(12, 12);
+            PenteTB.Name = "PenteTB";
+            PenteTB.RowCount = 19;
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.RowStyles.Add(new RowStyle(SizeType.Percent, 5.26315737F));
+            PenteTB.Size = new Size(900, 730);
+            PenteTB.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(944, 751);
+            Controls.Add(PenteTB);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel PenteTB;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Pente
 {
     public partial class Form1 : Form
@@ -5,6 +7,30 @@ namespace Pente
         public Form1()
         {
             InitializeComponent();
+            buttonCreation();
         }
+
+        public void buttonCreation()
+        {
+            for (int i = 0; i < 361; i++)
+            {
+                Button button = new Button();
+                button.Text = i.ToString();
+                button.Location = new Point(i, i);
+                button.Click += Pente_Click;
+                PenteTB.Controls.Add(button);
+            }
+        }
+
+        private void Pente_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            button.BackColor = Color.Black;
+
+            
+        }
+
+        //2d array for checking location
     }
 }
