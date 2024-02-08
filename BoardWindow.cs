a5639601204c620e799702dfea366e20176c1304
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2024-02-08 10:19:04 -0700

    Board Checking 50%

--- BoardWindow.cs
+++ BoardWindow.cs
@@ -52,7 +52,7 @@ namespace Pente
             {
                 if (button.Tag == i.ToString())
                 {
-                    Debug.WriteLine(":)");
+                    //Debug.WriteLine(":)");
                     break;
                 }
             }
@@ -67,6 +67,8 @@ namespace Pente
                     TableLayoutPanelCellPosition pos = Table.GetPositionFromControl(button);
                     grid[pos.Column, pos.Row] = 1;
 
+                    CheckWin();
+
                     player = 2;
                     textBox1.Text = "Player 2's Turn";
                 }
@@ -77,6 +79,8 @@ namespace Pente
                     // Change 2D Ary
                     TableLayoutPanelCellPosition pos = Table.GetPositionFromControl(button);
                     grid[pos.Column, pos.Row] = 2;
+
+                    CheckWin();
 
                     textBox1.Text = "Player 1's Turn";
                     player = 1;
@@ -89,18 +93,147 @@ namespace Pente
             return grid = new int[x, y];
         }
 
-        public void CheckWin()
+        public bool CheckWin()
         {
-            int row = grid.GetLength(0);
-            int col = grid.GetLength(1);
-
-            for (int i = 0; i < row; i++)
+            // check for 5 in row
+            for (int row = 0; row < grid.GetLength(0); row++)
             {
-                for (int j = 0; j < col; j++)
+                int symbolsInRow = 0;
+                for (int col = 0; col < grid.GetLength(1); col++)
                 {
-                    
+                    if (grid[row, col] == player)
+                    {
+                        symbolsInRow++;
+                        if (symbolsInRow == 5)
+                        {
+                            Debug.WriteLine(";)");
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        symbolsInRow = 0;
+                    }
                 }
             }
+
+            // check for 5 in column
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                int symbolsInCol = 0;
+                for (int row = 0; row < grid.GetLength(0); row++)
+                {
+                    if (grid[row, col] == player)
+                    {
+                        symbolsInCol++;
+                        if (symbolsInCol == 5)
+                        {
+                            Debug.WriteLine(":)");
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        symbolsInCol = 0;
+                    }
+                }
+            }
+
+            // check for 5 in left to right diagonall
+            int ltrWin = 0;
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+                    if (grid[row, col] == player)
+                    {
+                        ltrWin++;
+                        bool ChainBroken = false;
+                        int strRow = row;
+                        int strCol = col;
+
+                        while (ltrWin < 5 && !ChainBroken)
+                        {
+                            strRow += 1;
+                            strCol += 1;
+                            // out of bounds check needs to be added
+
+                            if (grid[strRow, strCol] == player)
+                            {
+
+                                ltrWin++;
+                            }
+                            else
+                            { 
+                                ChainBroken = true;
+                            }
+                        }
+
+                        if (ltrWin == 5)
+                        {
+                            Debug.WriteLine(ltrWin);
+                        }
+                        else if (ChainBroken)
+                        {
+                            ltrWin = 0;
+                            //Debug.WriteLine("L");
+                        }
+                    }
+                    else
+                    {
+                        ltrWin = 0;
+                    }
+                }
+            }
+
+
+
+            // check for 5 in right to left diagonall
+            int rtlWin = 0;
+            for (int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+                    if (grid[row, col] == player)
+                    {
+                        rtlWin++;
+                        bool ChainBroken = false;
+                        int strRow = row;
+                        int strCol = col;
+
+                        while (rtlWin < 5 && !ChainBroken)
+                        {
+                            strRow -= 1;
+                            strCol -= 1;
+                            if (grid[strRow, strCol] == player)
+                            {
+                                rtlWin++;
+                            }
+                            else
+                            {
+                                ChainBroken = true;
+                            }
+                        }
+
+                        if (rtlWin == 5)
+                        {
+                            Debug.WriteLine(rtlWin);
+                        }
+                        else if (ChainBroken)
+                        {
+                            rtlWin = 0;
+                            //Debug.WriteLine("L");
+                        }
+                    }
+                    else
+                    {
+                        rtlWin = 0;
+                    }
+                }
+            }
+
+
+            return false;
         }
     }
 }
